8f3a69b577de2f85444f0fbc11f3a42f405b58b0
Caio <caioarthurlima18@gmail.com>
2025-05-24 19:33:23 -0300

    criate migration of users

---
+++ src/database/index.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+
+const databaseConfig = require('../configs/db');
+
+class Database {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        this.connection = new Sequelize(databaseConfig);
+
+
+    }
+}
+
+module.exports = new Database();
---
+++ src/database/migrations/20250524222621-create-members.js
@@ -0,0 +1,65 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Members", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      full_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      birth_date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      gender: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      photo: {
+        type: Sequelize.STRING,
+      },
+      egress: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      skills: {
+        type: Sequelize.JSON,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW")
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW")
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Members")
+  }
+};
